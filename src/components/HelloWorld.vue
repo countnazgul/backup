35d412348585a25a809ac47f5d7cca47b63cb6bb
Stefan Stoichev <stefan.stoichev@gmail.com>
2017-12-20 15:14:34 +0000

    working with serializeApp

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,54 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-
-    <el-select v-model="enigma.selectedApp" placeholder="Select" filterable @change="selectApp">
+  <div>
+    <el-select 
+      class="select"
+      v-model="enigma.selectedApp" 
+      placeholder="Please select an app" 
+      no-match-text="Nothing found"
+      no-data-text="There are no apps available or there is an error :'("
+      :disabled="selectDisabled" 
+      filterable
+      size="large"
+      @change="selectApp">
       <el-option
         v-for="app in enigma.apps"
-        :key="app.qDocName"
+        :key="app.qDocId"
         :label="app.qTitle"
         :value="app.qDocId">
       </el-option>
     </el-select>
-
+    <i class="el-icon-loading" v-bind:class="{ hidden: !selectDisabled }"></i>
+  </div>
+  <el-button 
+    type="primary" 
+    plain
+    v-bind:class="{ hidden: !enigma.selectedApp }"
+    v-on:click="serialize">Serialize</el-button>
 
   </div>
 </template>
 
 <script>
 // const WebSocket = require('ws');
+import app2json from "serializeapp";
+import FileSaver from "filesaver.js";
+import moment from "moment";
 const enigma = require("enigma.js");
 const schema = require("enigma.js/schemas/12.20.0.json");
 
-const session = enigma.create({
+var sessionConfig = {
   schema,
-  url: "ws://localhost:4848/app/engineData",
+  url: "ws://localhost:4848/app/",
   createSocket: url => new WebSocket(url),
   unsecure: true
-});
+};
 
 // console.log(session);
 
+// session.on("opened", () => console.log("Session: Opened"));
+// session.on("closed", () => console.log("Session: Closed"));
 // session.on('notification:OnConnected', (data) => console.log(data));
 // session.on('traffic:sent', data => console.log('sent:', data));
 // session.on('traffic:received', data => console.log('received:', data));
@@ -38,7 +58,9 @@ export default {
   data() {
     return {
       msg: "Welcome to Your Vue.js App",
+      selectDisabled: true,
       enigma: {
+        session: {},
         global: {},
         app: {},
         apps: {},
@@ -49,77 +71,84 @@ export default {
   mounted: function() {
     var _this = this;
 
-    openSession(_this)
+    openSession(_this, sessionConfig)
+      .then(function(session) {
+        return session.open();
+      })
       .then(function(global) {
         console.log("Session open 1");
         _this.enigma.global = global;
-        console.log(global)
+        // console.log(global)
         return global.qvVersion();
       })
       .then(function(version) {
-        return (_this.msg = version);
-      })
-      .then(function() {
+        _this.msg = version;
         return _this.enigma.global.getDocList();
       })
       .then(function(docLists) {
-        return (_this.enigma.apps = docLists);
+        _this.selectDisabled = false;
+        _this.enigma.apps = docLists;
       });
   },
   methods: {
     selectApp: function(appId) {
       var _this = this;
-
-      openSession(_this)
+      _this.selectDisabled = true;
+      openSession(_this, sessionConfig, appId)
+        .then(function(session) {
+          return session.open();
+        })
         .then(function(global) {
           console.log("Session open 2");
           _this.enigma.global = global;
-          console.log(global)
-          return global.qvVersion();
-        })
-        .then(function(version) {
-          return (_this.msg = version);
-        })
-        .then(function() {
-          return _this.enigma.global.getDocList();
-        })
-        .then(function(docLists) {
-          return (_this.enigma.apps = docLists);
-        })
-        .then(function() {
+          // console.log(global);
           return _this.enigma.global.openDoc(appId);
         })
         .then(function(enigmaApp) {
           _this.enigma.app = enigmaApp;
+          _this.selectDisabled = false;
           return _this.enigma.app.getAppProperties();
         })
         .then(function(props) {
           console.log(props);
         });
+    },
+    serialize: function() {
+      var _this = this;
+      app2json(_this.enigma.app).then(function(serialized) {
+        var stamp = moment().format('YYYY-MM-DD_hh-mm-ss');
+        var fileName = `${_this.enigma.app.id}_${stamp}.json`
+        serialized = JSON.stringify(serialized, null, 4)
+        
+        var blob = new Blob([serialized], {
+          type: "application/json;charset=utf-8"
+        });
+        FileSaver.saveAs(blob, fileName);
+      });
     }
   }
 };
 
-function openSession(_this) {
-  return closeSession(_this).then(function(test) {
-    return session.open();
-  });
-}
+function openSession(_this, sessionConfig, appId) {
+  if (_this.enigma.app.id != null) {
+    _this.enigma.global = {};
 
-function closeSession(_this) {
-  if (_this.enigma.global.id) {
-    return session.close().then(function() {
-      return new Promise(function(res, rej) {
-        return res("aaa");
-      });
+    return new Promise(function(res, rej) {
+      res(_this.enigma.app.session.close());
+    }).then(function() {
+      var localConfig = sessionConfig;
+      localConfig.url = localConfig.url + "" + appId;
+      return enigma.create(sessionConfig);
     });
   } else {
     return new Promise(function(res, rej) {
       console.log("nothing to close");
-      res("aaa");
+      res(enigma.create(sessionConfig));
     });
   }
 }
+
+function closeSession(_this, session, appId) {}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -138,5 +167,12 @@ li {
 }
 a {
   color: #42b983;
+}
+.hidden {
+  display: none;
+}
+
+.select {
+  width: 900px;
 }
 </style>
